015ee8d90fcfc0d247ea119518b13892b7d5cab3
rauumartinez03 <raul.us03@gmail.com>
2024-03-12 16:00:33 +0100

    Comentarios para codigo, borrando cosas

--- Tareas_LED/Core/Src/main.c
+++ Tareas_LED/Core/Src/main.c
@@ -47,7 +47,7 @@ typedef State (*StateFunc)(void);
 #define ADC_CONVERTED_DATA_BUFFER_SIZE 64
 #define DIGITAL_SCALE_12BITS ((uint32_t) 0xFFF)
 #define DAC_CONVERTED_DATA_BUFFER_SIZE 128
-#define PI 3.14159265358979323846
+#define PI 3.14159265358979323846 //No hay PI en C sin libreria
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -73,21 +73,9 @@ State lastState = S3;
 /* Variables for ADC conversion data */
 uint16_t aADCxConvertedData[ADC_CONVERTED_DATA_BUFFER_SIZE]; /* ADC group regular conversion data (array of data) */
 
-uint16_t Wave_LUT[DAC_CONVERTED_DATA_BUFFER_SIZE] = {
-    2048, 2149, 2250, 2350, 2450, 2549, 2646, 2742, 2837, 2929, 3020, 3108, 3193, 3275, 3355,
-    3431, 3504, 3574, 3639, 3701, 3759, 3812, 3861, 3906, 3946, 3982, 4013, 4039, 4060, 4076,
-    4087, 4094, 4095, 4091, 4082, 4069, 4050, 4026, 3998, 3965, 3927, 3884, 3837, 3786, 3730,
-    3671, 3607, 3539, 3468, 3394, 3316, 3235, 3151, 3064, 2975, 2883, 2790, 2695, 2598, 2500,
-    2400, 2300, 2199, 2098, 1997, 1896, 1795, 1695, 1595, 1497, 1400, 1305, 1212, 1120, 1031,
-    944, 860, 779, 701, 627, 556, 488, 424, 365, 309, 258, 211, 168, 130, 97,
-    69, 45, 26, 13, 4, 0, 1, 8, 19, 35, 56, 82, 113, 149, 189,
-    234, 283, 336, 394, 456, 521, 591, 664, 740, 820, 902, 987, 1075, 1166, 1258,
-    1353, 1449, 1546, 1645, 1745, 1845, 1946, 2047
-};
-
-uint16_t LUT_Cuadrado[5];
-uint16_t LUT1k[50];
-uint16_t LUT10k[5];
+uint16_t LUT_Cuadrado[5]; //Declaracion LookupTable de funcion cuadrada
+uint16_t LUT1k[50]; //Declaracion LookupTable de funcion senoidal 1khz
+uint16_t LUT10k[5]; //Declaracion LookupTable de funcion senoidal 10khz
 
 /* USER CODE END PV */
 
@@ -166,14 +154,14 @@ int main(void)
   MX_DAC1_Init();
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
-//  void DAC_func(void);
-//  void Gen_LUT_Cuadrado(void);
-  void Gen_LUT_Senoidal(void);
-  int j = 0;
-  for(int i = 0; i < 50; i = i + 10) {
-	LUT10k[j] = LUT1k[i];
-	j++;
-  }
+//  void Gen_LUT_Cuadrado(void); //Para generar la funcion cuadrada
+  void Gen_LUT_Senoidal(void); //Para generar la funcion senoidal 1k
+
+//  int j = 0;
+//  for(int i = 0; i < 50; i = i + 10) { //Coger 5 muestras de 1k = hacer 10k, comentar para 1k
+//	LUT10k[j] = LUT1k[i];
+//	j++;
+//  }
 
   /* USER CODE END 2 */
 
@@ -205,7 +193,7 @@ int main(void)
     }
 
 
-  //#########START DAC##########
+  //#########START DAC########## //Cambiar en funcion de la funcion a generar
     //if (HAL_DAC_Start_DMA(&hdac1, DAC_CHANNEL_1, (uint32_t *)LUT_Cuadrado, 5, DAC_ALIGN_12B_R) != HAL_OK)
     //if (HAL_DAC_Start_DMA(&hdac1, DAC_CHANNEL_1, (uint32_t *)LUT1k, 50, DAC_ALIGN_12B_R) != HAL_OK)
     if (HAL_DAC_Start_DMA(&hdac1, DAC_CHANNEL_1, (uint32_t *)LUT10k, 5, DAC_ALIGN_12B_R) != HAL_OK)
@@ -847,13 +835,7 @@ State S3_func(){
 
 //FUNCIONES DAC
 
-void DAC_func(void) {
-	for(int i = 0; i < 128; i++){
-		Wave_LUT[i] = DIGITAL_SCALE_12BITS;
-	}
-}
-
-void Gen_LUT_Cuadrado(void) {
+void Gen_LUT_Cuadrado(void) { //Funcion para generar señal cuadrada
 	for(int i = 0; i < 5; i++)
 	{
 		if(i < 2){
@@ -861,16 +843,17 @@ void Gen_LUT_Cuadrado(void) {
 		}
 		else {
 			LUT_Cuadrado[i] = 1830;
-			//Medida aproximada
+			//Medida aproximada 2V
 		}
 
 	}
 }
 
-void Gen_LUT_Senoidal(void) {
+void Gen_LUT_Senoidal(void) { //Funcion para generar señal senoidal 1k
 	for(int i = 0; i < 50; i++)
 	{
 		LUT1k[i] = 950 + (int)(950 * sin(2 * PI * i / 50));
+		//Medida aproximada para amplitud 2V, offset 1V
 	}
 }
 
